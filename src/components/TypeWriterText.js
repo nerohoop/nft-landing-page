6d9ea02e088c15947a7ddaf03e5037227ad25ed9
Jiang Xiao <nerohoop@gmail.com>
2022-09-24 18:11:10 +0100

    Create the home section

---
+++ src/components/TypeWriterText.js
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+import Typewriter from "typewriter-effect";
+import Button from "./Button";
+
+const Title = styled.h2`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  width: 80%;
+  color: ${(props) => props.theme.text};
+  align-self: flex-start;
+
+  span {
+    text-transform: uppercase;
+    font-family: "Akaya Telivigala", cursive;
+  }
+  .text-1 {
+    color: blue;
+  }
+  .text-2 {
+    color: orange;
+  }
+  .text-1 {
+    color: red;
+  }
+`;
+
+const Subtitle = styled.h3`
+  font-size: ${(props) => props.theme.fontlg};
+  font-weight: 600;
+  text-transform: capitalize;
+  color: ${(props) => `rgba(${props.theme.textRgba}, 0.6)`};
+  align-self: flex-start;
+  margin-bottom: 1rem;
+  width: 80%;
+`;
+
+const ButtonContainer = styled.div`
+  width: 80%;
+  align-self: flex-start;
+`;
+
+const TypeWriterText = () => {
+  return (
+    <>
+      <Title>
+        Discover a new era of cool
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString('<span class="text-1">NFTs.</span>')
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString('<span class="text-2">Collectible Items.</span>')
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString('<span class="text-3">Ape Killers.</span>')
+              .pauseFor(2000)
+              .deleteAll()
+              .start();
+          }}
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </Title>
+      <Subtitle>Bored of Apes? Try Something New.</Subtitle>
+      <ButtonContainer>
+        <Button text="Explore" link="#about" />
+      </ButtonContainer>
+    </>
+  );
+};
+
+export default TypeWriterText;

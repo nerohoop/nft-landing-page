21eabb2bd251020d985d487fbb053552d1799774
Jiang Xiao <nerohoop@gmail.com>
2022-10-04 00:17:05 +0100

    Finish Faq section

--- src/components/sections/FAQ.js
+++ src/components/sections/FAQ.js
@@ -1,7 +1,103 @@
-import React from "react";
+import React, { useRef, useLayoutEffect } from "react";
+import styled from "styled-components";
+import Accordion from "../Accordion";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.text};
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: uppercase;
+  color: ${(props) => props.theme.body};
+
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.carouselColor};
+  width: fit-content;
+`;
+
+const Container = styled.div`
+  width: 75%;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+`;
+
+const Box = styled.div`
+  width: 45%;
+  color: ${(props) => props.theme.body};
+`;
 
 const FAQ = () => {
-  return <div>FAQ</div>;
+  // const ref = useRef(null);
+  // gsap.registerPlugin(ScrollTrigger);
+  // useLayoutEffect(() => {
+  //   let element = ref.current;
+
+  //   ScrollTrigger.create({
+  //     trigger: element,
+  //     start: "top top",
+  //     end: "bottom top",
+  //     pin: true,
+  //     pinSpaceing: false,
+  //     scrub: true,
+  //   });
+
+  //   return () => {};
+  // }, []);
+
+  return (
+    <Section>
+      <Title>FAQ</Title>
+      <Container>
+        <Box>
+          <Accordion title="WHERE CAN I VIEW MY NFTS?">
+            Once minted or bought simply connect to your OpenSea account to view
+            your NFTs.
+          </Accordion>
+          <Accordion title="WHAT IS THE METAVERSE?">
+            A metaverse is a network of 3D virtual worlds focused on social
+            connection. In futurism and science fiction, it is often described
+            as a hypothetical iteration of the Internet as a single, universal
+            virtual world that is facilitated by the use of virtual and
+            augmented reality headsets.
+          </Accordion>
+          <Accordion title="WHY DO WE NEED ROYALTIES?">
+            The amount of royalties was fixed at 5% to finance the Weirdos
+            Club's projects. We have the ambition to organize multiple events
+            around the world in order to strengthen the community and build a
+            network of entrepreneurs and investors with the same mindset and
+            common interests.
+          </Accordion>
+        </Box>
+        <Box>
+          <Accordion title="WHERE CAN I VIEW MY NFTS?">
+            Once minted or bought simply connect to your OpenSea account to view
+            your NFTs.
+          </Accordion>
+          <Accordion title="WHERE CAN I VIEW MY NFTS?">
+            Once minted or bought simply connect to your OpenSea account to view
+            your NFTs.
+          </Accordion>
+          <Accordion title="WHERE CAN I VIEW MY NFTS?">
+            Once minted or bought simply connect to your OpenSea account to view
+            your NFTs.
+          </Accordion>
+        </Box>
+      </Container>
+    </Section>
+  );
 };
 
 export default FAQ;
